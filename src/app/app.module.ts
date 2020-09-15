4aa23b43645bec7ce7dc572c020b4882c120f83b
Philip Dion Leo <phil3947@elev.tec.dk>
2020-09-15 08:22:13 +0200

    more navbar?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-=======
 import { ClimateComponent } from './components/climate/climate.component';
 import { ListComponent } from './components/list/list.component';
->>>>>>> c89ddd76ba3994da6b3575c96c5d1577b73515ba
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    NavBarComponent
-=======
+    NavBarComponent,
     ClimateComponent,
     ListComponent
->>>>>>> c89ddd76ba3994da6b3575c96c5d1577b73515ba
   ],
   imports: [
     BrowserModule,
